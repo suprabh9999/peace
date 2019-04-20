0cd7d26b8f07f574ec5a3f150b6a5f63478e71d1
Mona Lisa <official.suprabh@gmail.com>
2019-04-20 11:15:56 +0530

    Updates

---
+++ precache-manifest.ac2ba971fc3cfb8d37f64d6e950e5115.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f37ca4e4257e966dbb7",
+    "url": "/peace/static/js/runtime~main.43924a41.js"
+  },
+  {
+    "revision": "5844315dde43b94e33d5",
+    "url": "/peace/static/js/main.421d72c8.chunk.js"
+  },
+  {
+    "revision": "1d7401b70a0f97cc8d15",
+    "url": "/peace/static/js/2.ec8c139e.chunk.js"
+  },
+  {
+    "revision": "5844315dde43b94e33d5",
+    "url": "/peace/static/css/main.1fb41da6.chunk.css"
+  },
+  {
+    "revision": "f474156e3c37526b7dcc7d336f1b7975",
+    "url": "/peace/index.html"
+  }
+];
